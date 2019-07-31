d2f5bd9df0414c34ae40b00fe32ae01fb9a4cce3
zwjss <ss13806435635@163.com>
2019-07-31 16:24:23 +0800

    搭建数据库的一系列操作，搭建了BLOG文章的逻辑主体结构

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# zwjss_blog
+## zwjss_blog
+>note：7月30日开始的django BLOG
+
+# step1:
+  参考zmrenwu教程自己搭建zwjss_blog
--- zwjss_blog/settings.py
+++ zwjss_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'zwjss_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zwjss_blog',
+        'USER': 'root',
+        'PASSWORD': 'sunshuo123',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'autocommit': True,
+        },
     }
 }
 
@@ -104,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
